0304ccb6cd2ffe9017e9a130dde6080a01d51ac0
Dao Hai Nam <daohainam@gmail.com>
2025-03-01 12:02:29 +0100

    Add BorrowingHistoryApi project and update configurations

--- MicroservicePatterns.AppHost/Program.cs
+++ MicroservicePatterns.AppHost/Program.cs
@@ -4,4 +4,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddApplicationServices();
 
+builder.AddProject<Projects.CQRS_Library_OrderingHistoryApi>("cqrs-library-orderinghistoryapi");
+
+builder.AddProject<Projects.CQRS_Library_BorrowingHistoryApi>("cqrs-library-borrowinghistoryapi");
+
 builder.Build().Run();
