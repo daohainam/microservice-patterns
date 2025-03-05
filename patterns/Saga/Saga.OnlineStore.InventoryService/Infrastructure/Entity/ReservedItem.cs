c08405e14c8f35f4bea9b93a35da860250dcad0e
Dao Hai Nam <daohainam@gmail.com>
2025-03-05 19:22:35 +0100

    Add migrations to Saga projects

--- patterns/Saga/Saga.OnlineStore.InventoryService/Infrastructure/Entity/ReservedItem.cs
+++ patterns/Saga/Saga.OnlineStore.InventoryService/Infrastructure/Entity/ReservedItem.cs
@@ -1,7 +1,11 @@
-﻿namespace Saga.OnlineStore.InventoryService.Infrastructure.Entity;
+﻿using System;
 
+namespace Saga.OnlineStore.InventoryService.Infrastructure.Entity;
+
+[Index(nameof(OrderId), nameof(ItemId), IsUnique = true)]
 public class ReservedItem
 {
+    public Guid Id { get; set; }
     public Guid OrderId { get; set; }
     public Guid ItemId { get; set; }
     public long Quantity { get; set; }
---
+++ patterns/Saga/Saga.OnlineStore.InventoryService/Migrations/20250305182105_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Saga.OnlineStore.InventoryService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AvailableQuantity = table.Column<long>(type: "bigint", nullable: false),
+                    ReservedQuantity = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReservedItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ItemId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Quantity = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReservedItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservedItems_OrderId_ItemId",
+                table: "ReservedItems",
+                columns: new[] { "OrderId", "ItemId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "ReservedItems");
+        }
+    }
+}
