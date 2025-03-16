01b7085851534d3d04763b948a451e9da8070bb1
Dao Hai Nam <daohainam@gmail.com>
2025-03-16 14:37:22 +0100

    Add booking management endpoints to HotelApi
    Updated HotelApiExtensions to include new API routes for
    creating and confirming bookings. Changed CreateRoom method
    visibility to internal. Introduced CreateBooking and
    ConfirmBooking methods with necessary logic and logging
    for handling bookings.

--- patterns/Saga/Saga.TripPlanner.HotelService/Apis/HotelApi.cs
+++ patterns/Saga/Saga.TripPlanner.HotelService/Apis/HotelApi.cs
@@ -1,4 +1,5 @@
-﻿namespace Saga.TripPlanner.HotelService.Apis;
+﻿
+namespace Saga.TripPlanner.HotelService.Apis;
 public static class HotelApiExtensions
 {
     public static IEndpointRouteBuilder MapHotelApi(this IEndpointRouteBuilder builder)
@@ -24,12 +25,16 @@ public static class HotelApiExtensions
 
         group.MapPost("rooms", HotelApi.CreateRoom);
 
+        group.MapPost("rooms/{id:guid}/bookings", HotelApi.CreateBooking);
+        group.MapPut("bookings/{bookingId:guid}/confirm", HotelApi.ConfirmBooking);
+
+
         return group;
     }
 }
 public class HotelApi
 { 
-    public static async Task<Results<Ok<Room>, BadRequest>> CreateRoom([AsParameters] ApiServices services, Room room)
+    internal static async Task<Results<Ok<Room>, BadRequest>> CreateRoom([AsParameters] ApiServices services, Room room)
     {
         if (room == null) {
             return TypedResults.BadRequest();
@@ -42,6 +47,65 @@ public class HotelApi
         await services.DbContext.SaveChangesAsync();
 
         return TypedResults.Ok(room);
+    }
+
+    internal static async Task<Results<Ok<Booking>, BadRequest, NotFound>> CreateBooking([AsParameters] ApiServices services, Guid id, Booking booking)
+    {
+        if (id == Guid.Empty)
+        {
+            return TypedResults.BadRequest();
+        }
+
+        var room = await services.DbContext.Rooms.FindAsync(id);
+        if (room == null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        // we need to make sure that the room is available for the booking dates
+        var bookings = room.Bookings.Where(b => b.CheckInDate <= booking.CheckOutDate && b.CheckOutDate >= booking.CheckInDate).ToList();
+
+        if (bookings.Count != 0)
+        {
+            services.Logger.LogInformation("Room is not available for the booking dates");
+            return TypedResults.BadRequest();
+        }
+
+        if (booking.Id == Guid.Empty)
+            booking.Id = Guid.CreateVersion7();
+
+        booking.RoomId = id;
+        booking.BookingDate = DateTime.UtcNow;
+        booking.Status = BookingStatus.Pending;
+        bookings.Add(booking);
+
+        await services.DbContext.SaveChangesAsync();
+
+        services.Logger.LogInformation("Booking {id} created successfully", booking.Id);
+
+        return TypedResults.Ok(booking);
+    }
+
+    internal static async Task<Results<Ok<Booking>, BadRequest, NotFound>> ConfirmBooking([AsParameters] ApiServices services, Guid bookingId)
+    {
+        var booking = await services.DbContext.Bookings.FindAsync(bookingId);
+        if (booking == null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        if (booking.Status != BookingStatus.Pending)
+        {
+            services.Logger.LogInformation("Booking {id} is not in pending state", booking.Id);
+            return TypedResults.Ok(booking);
+        }
+
+        booking.Status = BookingStatus.Confirmed;
+        
+        await services.DbContext.SaveChangesAsync();
+        services.Logger.LogInformation("Booking {id} confirmed successfully", booking.Id);
+
+        return TypedResults.Ok(booking);
     }
 
 }
