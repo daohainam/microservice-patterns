fc4c4a2a3191911213d4ff33bba3ecdb5c626e93
daohainam <daohainam@gmail.com>
2025-04-01 10:02:29 +0200

    Add integration events and update project configuration
    Introduced `AccountOpenedIntegrationEvent` and `BalanceChangedIntegrationEvent` classes for account management. Updated `EventSourcing.Banking.IntegrationEvents.csproj` to target .NET 9.0, enable implicit usings, and added a reference to the `EventBus` project.

---
+++ patterns/EventSourcing/EventSourcing.Banking.IntegrationEvents/AccountIntegrationEvents.cs
@@ -0,0 +1,11 @@
+﻿using EventBus.Events;
+
+namespace EventSourcing.Banking.IntegrationEvents;
+
+public class AccountOpenedIntegrationEvent: IntegrationEvent
+{
+    public string AccountNumber { get; set; } = default!;
+    public string Currency { get; set; } = default!;
+    public decimal Balance { get; set; }
+    public decimal CreditLimit { get; set; }
+}
---
+++ patterns/EventSourcing/EventSourcing.Banking.IntegrationEvents/BalanceIntegrationEvents.cs
@@ -0,0 +1,10 @@
+﻿using EventBus.Events;
+
+namespace EventSourcing.Banking.IntegrationEvents;
+
+public class BalanceChangedIntegrationEvent: IntegrationEvent
+{
+    public string AccountNumber { get; set; } = default!;
+    public decimal Balance { get; set; }
+    public decimal Credit { get; set; }
+}
