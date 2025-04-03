bb507b961077019847d4ed9cc97b3a2c9a0a3ffc
daohainam <daohainam@gmail.com>
2025-04-03 16:13:14 +0200

    Implement Outbox pattern using Debezium

---
+++ patterns/TransactionalOutbox/TransactionalOutbox.Publisher.Debezium/DebeziumResource.cs
@@ -0,0 +1,22 @@
+﻿namespace TransactionalOutbox.Publisher.Debezium
+{
+    public sealed class DebeziumResource(string name) : ContainerResource(name), IResourceWithConnectionString
+    {
+        internal const string HttpEndpointName = "http";
+        internal const string TcpEndpointName = "tcp";
+        internal const int DefaultHttpPort = 2113;
+        internal const int DefaultTcpPort = 1113;
+
+        // An EndpointReference is a core .NET Aspire type used for keeping
+        // track of endpoint details in expressions. Simple literal values cannot
+        // be used because endpoints are not known until containers are launched.
+        private EndpointReference? _httpEndPointReference;
+
+        public EndpointReference DebeziumEndpoint =>
+            _httpEndPointReference ??= new(this, HttpEndpointName);
+        public ReferenceExpression ConnectionStringExpression =>
+            ReferenceExpression.Create(
+                $"esdb://{DebeziumEndpoint.Property(EndpointProperty.Host)}:{DebeziumEndpoint.Property(EndpointProperty.Port)}?tls=false"
+            );
+    }
+}
---
+++ patterns/TransactionalOutbox/TransactionalOutbox.Publisher.Debezium/DebeziumResourceBuilderExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Aspire.Hosting;
+
+namespace TransactionalOutbox.Publisher.Debezium
+{
+    public static class DebeziumResourceBuilderExtensions
+    {
+        public static IResourceBuilder<DebeziumResource> AddEventStore(
+        this IDistributedApplicationBuilder builder,
+        string name,
+        int? httpPort = null,
+        int? tcpPort = null)
+        {
+            var resource = new DebeziumResource(name);
+
+            return builder.AddResource(resource)
+                          .WithImage(DebeziumContainerImageTags.Image)
+                          .WithImageRegistry(DebeziumContainerImageTags.Registry)
+                          .WithImageTag(DebeziumContainerImageTags.Tag)
+                          .WithEndpoint(port: tcpPort, targetPort: DebeziumResource.DefaultTcpPort, name: DebeziumResource.TcpEndpointName)
+                          .WithHttpEndpoint(port: httpPort, targetPort: DebeziumResource.DefaultHttpPort, name: DebeziumResource.HttpEndpointName)
+                          .WithEnvironment(ConfigureDebeziumContainer);
+        }
+
+        private static void ConfigureDebeziumContainer(EnvironmentCallbackContext context)
+        {
+        }
+
+        public static IResourceBuilder<DebeziumResource> WithKafka(this IResourceBuilder<DebeziumResource> builder, string server)
+        {
+            return builder
+                .WithEnvironment("KAFKA_CONNECT_BOOTSTRAP_SERVERS", server);
+        }
+
+        public static IResourceBuilder<DebeziumResource> WithDataVolume(this IResourceBuilder<DebeziumResource> builder)
+        {
+            return builder
+                .WithVolume("debezium-volume-data", "/var/lib/debezium-data");
+        }
+        public static IResourceBuilder<DebeziumResource> WithLogsVolume(this IResourceBuilder<DebeziumResource> builder)
+        {
+            return builder
+                .WithVolume("debezium-volume-logs", "/var/log/debezium")
+                ;
+        }
+    }
+
+    internal static class DebeziumContainerImageTags
+    {
+        internal const string Registry = "quay.io";
+
+        internal const string Image = "debezium/connect";
+
+        internal const string Tag = "latest";
+    }
+}
