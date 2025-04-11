f5aee1933659d66c8c96459e8580380951a3d3ca
daohainam <daohainam@gmail.com>
2025-04-11 14:51:58 +0200

    Add webhook API functionality and enforce HTTPS
    - Introduced new endpoint mapping for webhook API in Program.cs.
    - Updated launchSettings.json to enforce HTTPS only.
    - Modified WebHook.DeliveryService.http for new webhook endpoint and request structure.
    - Added WebHookApi class with methods for registering and unregistering webhooks, including URL validation and secret key generation.

---
+++ patterns/WebHook/WebHook.DeliveryService/Apis/WebHookApi.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.Json;
+using WebHook.DeliveryService.DomainEvents;
+using WebHook.DeliveryService.Infrastructure.Entity;
+
+namespace WebHook.DeliveryService.Apis;
+public static class WebHookApiExtensions
+{
+    public static IEndpointRouteBuilder MapWebHookApi(this IEndpointRouteBuilder builder)
+    {
+        builder.MapGroup("/api/webhook/v1")
+              .MapWebHookApi()
+              .WithTags("WebHook Api");
+
+        return builder;
+    }
+    public static RouteGroupBuilder MapWebHookApi(this RouteGroupBuilder group)
+    {
+        group.MapPost("webhooks", WebHookApi.RegisterWebHook);
+
+        group.MapPut("webhooks/{id:guid}/unregister", WebHookApi.UnregisterWebHook);
+
+        return group;
+    }
+}
+public class WebHookApi
+{ 
+    internal static async Task<Results<Ok<WebHookSubscription>, BadRequest, BadRequest<string>>> RegisterWebHook([AsParameters] ApiServices services, WebHookRegisterRequest webHook)
+    {
+        if (webHook == null) {
+            return TypedResults.BadRequest();
+        }
+
+        if (string.IsNullOrWhiteSpace(webHook.Url))
+        {
+            return TypedResults.BadRequest("Url is required");
+        }
+
+        if (!Uri.TryCreate(webHook.Url, UriKind.Absolute, out var uriResult) || (uriResult.Scheme != Uri.UriSchemeHttp && uriResult.Scheme != Uri.UriSchemeHttps))
+        {
+            return TypedResults.BadRequest("Invalid Url format");
+        }
+
+        var webHookSubscription = new WebHookSubscription()
+        {
+            Id = Guid.CreateVersion7(),
+            Url = webHook.Url,
+            SecretKey = GenerateRandomString(),
+        };
+
+        await services.DbContext.WebHookSubscriptions.AddAsync(webHookSubscription);
+
+        await services.DbContext.SaveChangesAsync();
+
+        return TypedResults.Ok(webHookSubscription);
+    }
+
+    private const string AlphanumericCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static string GenerateRandomString(int length = 16)
+    {
+        var random = new Random();
+        return new string([.. Enumerable.Repeat(AlphanumericCharacters, length).Select(s => s[random.Next(s.Length)])]);
+    }
+
+    internal static async Task<Results<NotFound, Ok>> UnregisterWebHook([AsParameters] ApiServices services, Guid id, WebHookSubscription webHook)
+    {
+        var webHookSubscription = await services.DbContext.WebHookSubscriptions.Where(wh => wh.Id == id && wh.Url == webHook.Url && wh.SecretKey == webHook.SecretKey).SingleOrDefaultAsync();
+        if (webHookSubscription == null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        services.DbContext.Entry(webHookSubscription).State = EntityState.Deleted;
+
+        await services.DbContext.SaveChangesAsync();
+
+        return TypedResults.Ok();
+    }
+}
+
+public class WebHookRegisterRequest
+{
+    public string Url { get; set; } = default!;
+}
--- patterns/WebHook/WebHook.DeliveryService/Program.cs
+++ patterns/WebHook/WebHook.DeliveryService/Program.cs
@@ -13,6 +13,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 app.MapDeliveryServiceApi();
+app.MapWebHookApi();
 
 await app.MigrateDbContextAsync<DeliveryServiceDbContext>();
 
