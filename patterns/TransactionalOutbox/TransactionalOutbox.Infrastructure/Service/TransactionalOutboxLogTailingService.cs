0fba94785b2209abc2c65694d5eebff4413cfa1f
Dao Hai Nam <daohainam@gmail.com>
2025-10-25 10:33:03 +0200

    Update dependencies and fix minor bugs

--- patterns/TransactionalOutbox/TransactionalOutbox.Infrastructure/Service/TransactionalOutboxLogTailingService.cs
+++ patterns/TransactionalOutbox/TransactionalOutbox.Infrastructure/Service/TransactionalOutboxLogTailingService.cs
@@ -80,7 +80,7 @@ internal class TransactionalOutboxLogTailingService : BackgroundService
 
     private IntegrationEvent? RebuildEvent(LogTailingOutboxMessage message)
     {
-        var type = options.PayloadTypeRsolver(message.PayloadType);
+        var type = options.PayloadTypeResolver(message.PayloadType);
 
         if (type == null)
         {
@@ -95,6 +95,6 @@ internal class TransactionalOutboxLogTailingService : BackgroundService
 
 public class TransactionalOutboxLogTailingServiceOptions
 {
-    public Func<string, Type?> PayloadTypeRsolver { get; set; } = Type.GetType; 
+    public Func<string, Type?> PayloadTypeResolver { get; set; } = Type.GetType; 
     public required string ConnectionString { get; set; } 
 }
